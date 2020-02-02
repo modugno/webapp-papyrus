08cd3462103cc9d8d2c82d0f03478d0252451d70
Guilherme Modugno <g.modugno@hotmail.com>
2020-02-02 18:11:15 -0200

    chore(modules): add modules and lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommentModule } from './modules/comment/comment.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    redirectTo: 'discovery'
+  },
+  {
+    path: 'discovery',
+    loadChildren: () => import('./modules/discovery/discovery.module').then((m) => m.DiscoveryModule)
+  },
+  {
+    path: 'comment',
+    loadChildren: () => import('./modules/comment/comment.module').then((m) => m.CommentModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/discovery/discovery.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DiscoveryRoutingModule } from './discovery-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DiscoveryRoutingModule
+  ]
+})
+export class DiscoveryModule { }
